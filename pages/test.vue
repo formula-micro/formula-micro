91e1e3e0db01f12681b0dc5cafe7bcad7cf49cf5
MB <mb@formula-micro.dk>
2024-08-07 08:51:06 +0200

    Added a test page, in order to test preview event.

---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    // Fields.
+    const { $preview } = useNuxtApp(); 
+    const previewData = ref({} as any);
+
+    if ($preview && import.meta.client) {
+        window.addEventListener("message", (event) =>
+        {
+            const { type, path, values } = event.data;
+            if (type !== "directus-preview") return;
+            previewData.value = event.data;
+            console.log(event.data);
+        }, false);
+    }
+</script>
+
+<template>
+    <div>
+        {{ previewData }}
+    </div>
+</template>
