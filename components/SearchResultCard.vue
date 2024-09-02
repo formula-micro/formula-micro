fe7cd331ba55b0a409d5623bd8dc6df48f94e907
MB <mb@formula-micro.dk>
2024-09-02 15:51:04 +0200

    Fixed Breadcrumb styling issue.

--- components/SearchResultCard.vue
+++ components/SearchResultCard.vue
@@ -13,7 +13,7 @@
 <template>
     <NuxtLink :to="`/${to}`" class="focus:outline-none group">
         <div class="flex flex-col transition duration-150 bg-gray-50 border border-gray-100 group-hover:bg-gray-100 group-focus:bg-gray-100 group-focus:ring-2 ring-offset-2 ring-black rounded-xl p-3">
-            <Breadcrumb :model="breadcrumbs.map(breadcrumb => ({ label: breadcrumb }))" pt:root="bg-transparent text-xs px-0 py-2 select-none">
+            <Breadcrumb :model="breadcrumbs.map(breadcrumb => ({ label: breadcrumb }))" pt:root="!bg-transparent text-xs !px-0 !py-2 select-none">
                 <template #item="{ item }">
                     <p class="font-medium tracking-wide">{{ item.label }}</p>
                 </template>
