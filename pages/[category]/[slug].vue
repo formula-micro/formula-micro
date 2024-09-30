3a643f2dea6bb10317e79cefc3b7da7bece10ff3
MB <mb@formula-micro.dk>
2024-09-30 14:24:27 +0200

    Latest changes.

--- pages/[category]/[slug].vue
+++ pages/[category]/[slug].vue
@@ -38,35 +38,35 @@
     <div class="flex flex-col items-center w-full mt-5 xl:mt-12">
         <div class="flex flex-col justify-center items-center w-full">
             <template v-for="block in blocks">
-                <BlocksBannerHero v-if="block.__typename === 'banner_hero_blocks'" :data="block" />
+                <LazyBlocksBannerHero v-if="block.__typename === 'banner_hero_blocks'" :data="block" />
 
-                <BlocksTextHero v-if="block.__typename === 'text_hero_blocks'" :data="block" />
+                <LazyBlocksTextHero v-if="block.__typename === 'text_hero_blocks'" :data="block" />
 
-                <BlocksGallery v-if="block.__typename === 'gallery_blocks'" :data="block" />
+                <LazyBlocksGallery v-if="block.__typename === 'gallery_blocks'" :data="block" />
 
-                <BlocksPartners v-if="block.__typename === 'partners_blocks'" :data="block" />
+                <LazyBlocksPartners v-if="block.__typename === 'partners_blocks'" :data="block" />
 
-                <BlocksServicesList v-if="block.__typename === 'services_list_blocks'" :data="block" />
+                <LazyBlocksServicesList v-if="block.__typename === 'services_list_blocks'" :data="block" />
 
-                <BlocksTextAndImage v-if="block.__typename === 'text_and_image_blocks'" :data="block" />
+                <LazyBlocksTextAndImage v-if="block.__typename === 'text_and_image_blocks'" :data="block" />
 
-                <BlocksRichText v-if="block.__typename === 'richtext_blocks'" :data="block" />
+                <LazyBlocksRichText v-if="block.__typename === 'richtext_blocks'" :data="block" />
 
-                <BlocksSpacer v-if="block.__typename === 'spacer_blocks'" :data="block" />
+                <LazyBlocksSpacer v-if="block.__typename === 'spacer_blocks'" :data="block" />
 
-                <BlocksDisclosureGroup v-if="block.__typename === 'disclosure_group_blocks'" :data="block" />
+                <LazyBlocksDisclosureGroup v-if="block.__typename === 'disclosure_group_blocks'" :data="block" />
 
-                <BlocksDisclosure v-if="block.__typename === 'disclosure_blocks'" :is-child="false" :data="block" />
+                <LazyBlocksDisclosure v-if="block.__typename === 'disclosure_blocks'" :is-child="false" :data="block" />
 
-                <BlocksTabs v-if="block.__typename === 'tabs_blocks'" :data="block" />
+                <LazyBlocksTabs v-if="block.__typename === 'tabs_blocks'" :data="block" />
 
-                <BlocksGoogleMaps v-if="block.__typename === 'google_maps_blocks'" :data="block" />
+                <LazyBlocksGoogleMaps v-if="block.__typename === 'google_maps_blocks'" :data="block" />
 
-                <BlocksCookieSelection v-if="block.__typename === 'cookie_selection_blocks'" :data="block" />
+                <LazyBlocksCookieSelection v-if="block.__typename === 'cookie_selection_blocks'" :data="block" />
 
-                <BlocksContactForm v-if="block.__typename === 'contact_form_blocks'" :data="block" />
+                <LazyBlocksContactForm v-if="block.__typename === 'contact_form_blocks'" :data="block" />
 
-                <BlocksColumns v-if="block.__typename === 'columns_blocks'" :data="block" />
+                <LazyBlocksColumns v-if="block.__typename === 'columns_blocks'" :data="block" />
             </template>
         </div>
     </div>
